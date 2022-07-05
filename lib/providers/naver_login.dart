2e3c8244dc2445f74642a182dd73ffe41db85921
PlugDev77 <ochh508@naver.com>
2022-07-06 01:43:17 +0900

    네이버 로그인 기능 구현

--- lib/providers/naver_login.dart
+++ lib/providers/naver_login.dart
@@ -3,13 +3,41 @@ import 'package:flutter_naver_login/flutter_naver_login.dart';
 
 class NaverLoginProvider with ChangeNotifier {
   late String nName, nGender, nBirth;
+  // final Uri loginApiUrl = Uri.parse('http://p-cube-plus.com/auth/naver/login');
 
-  void naverLogin() async {
+  Future<void> naverLogin() async {
+    //if (!await launchUrl(loginApiUrl)) throw 'Could not launch $loginApiUrl';
+
     NaverLoginResult res = await FlutterNaverLogin.logIn();
+
+    switch (res.status) {
+      case NaverLoginStatus.loggedIn:
+        // data 처리
+        nName = res.account.name;
+        nGender = res.account.birthyear;
+        nBirth = res.account.birthday;
+
+        debugPrint("test " + nName);
+        debugPrint("test " + nGender);
+        debugPrint("test " + nBirth);
+
+        // retrofit으로 판큐 회원인지 검증해야됨
+        // 아니면 탈퇴처리
+        // UI에 뿌려주는 거
+
+        break;
+      case NaverLoginStatus.cancelledByUser:
+        // 유저에 의해 cancel
+        break;
+      case NaverLoginStatus.error:
+        // error
+        break;
+    }
+
     // data 처리
-    nName = res.account.nickname;
-    nGender = res.account.gender;
-    nBirth = res.account.birthday;
+    //nName = res.account.nickname;
+    //nGender = res.account.gender;
+    //nBirth = res.account.birthday;
 
     //notifyListeners();
   }
