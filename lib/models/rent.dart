14daf69dc61fdbbf2ea2a646ef5d543eb67ec76a
seungbin0619 <seungbin0619@naver.com>
2022-09-21 18:06:33 +0900

    홈화면 rent ui

--- lib/models/rent.dart
+++ lib/models/rent.dart
@@ -1,28 +1,38 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import 'product.dart';
+
 @JsonSerializable()
 class Rent {
   final int id;
-  final String name;
+  final int dDay;
+  final DateTime date;
   final DateTime deadline;
+  final Product product;
 
   Rent({
     required this.id,
-    required this.name,
+    required this.dDay,
+    required this.date,
     required this.deadline,
+    required this.product,
   });
 
   factory Rent.fromJson(Map<String, dynamic> json) {
     return Rent(
       id: json['id'],
-      name: json['name'],
+      dDay: json['d_day'],
+      date: DateTime.parse(json['rent_day']),
       deadline: DateTime.parse(json['deadline']),
+      product: Product.fromJson(json['product']),
     );
   }
 
   Map<String, dynamic> toJson() => {
         'id': id,
-        'name': name,
+        'd_day': dDay,
+        'rent_day': date.toString(),
         'deadline': deadline.toString(),
+        'product': product.toJson(),
       };
 }
--- lib/providers/rent_provider.dart
+++ lib/providers/rent_provider.dart
@@ -2,41 +2,55 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../services/rent_api.dart';
 import '../models/rent.dart';
 
 class RentProvider with ChangeNotifier {
+  late RentListApi _client;
+
   bool loaded = false;
   bool fail = false;
 
   late List<Rent>? _rentList;
-  List<Rent>? get rentList => _rentList;
+  List<Rent>? get rentList => loaded ? _rentList : null;
 
-  ScheduleProvider() {
+  RentProvider() {
     initialize();
   }
 
   void initialize() {
     update();
-    loaded = true;
   }
 
   Future<List<Rent>?> _getRents() async {
-    //String js = jsonEncode(tempRentList());
-    //Map<String, dynamic> json = jsonDecode(js);
+    _client = RentListApi();
+    List<Rent>? _rentList = await _client.getRentList();
 
-    return tempRentList();
+    //_client = RentListApi(id: 1);
+    //Rent? _rent = await _client.getRentInformation();
+    //print(_rent!.toJson().toString());
+
+    return _rentList;
   }
 
-  List<Rent> tempRentList() {
-    return <Rent>[];
+  Rent? getRent({int? id, int? index}) {
+    if (!loaded) return null;
+    if (id == null && index == null) return null;
+    if (index != null) return _rentList![index];
+
+    var rents = _rentList!.where((e) => e.id == id);
+
+    return rents.isEmpty ? null : rents.first;
   }
 
   void update() async {
-    if (!loaded) return;
-
     // 데이터 갱신
     _rentList = await _getRents();
+
     fail = _rentList == null;
+    loaded = true;
+
+    //print(getRent(id: 1)!.toJson());
 
     notifyListeners();
   }
