0fc54ec1aa7189fd5b0f59e936de2e3a8defa526
Retrobas <ohminkwon2718@gmail.com>
2023-03-08 16:05:32 +0900

    파일 정리
    불필요한 dart 파일 삭제

--- lib/screens/notice/notice_page.dart
+++ lib/screens/notice/notice_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:p_cube_plus_application/widgets/default/default_page.dart';
-import 'package:p_cube_plus_application/widgets/notice_box_widget.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
 import '../../models/notification_node.dart';
 import '../../widgets/default/default_tabbar.dart';
+import '../../widgets/default/rounded_border.dart';
 import '../settings/setting_notice_page.dart';
 
 Future<List<NotificationNode>> fetchNotification() async {
@@ -103,6 +104,72 @@ class NoticeListView extends StatelessWidget {
         // 기본적으로 로딩 Spinner를 보여줍니다.
         return Center(child: CircularProgressIndicator());
       },
+    );
+  }
+}
+
+class NoticeBoxWidget extends StatelessWidget {
+  const NoticeBoxWidget({required this.isNew, required this.box});
+
+  final NotificationNode box;
+  final bool isNew;
+
+  @override
+  Widget build(BuildContext context) {
+    return RoundedBorder(
+      onTap: () {},
+      hasShadow: true,
+      color: isNew
+          ? Theme.of(context).bottomNavigationBarTheme.backgroundColor
+          : Theme.of(context).disabledColor,
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    box.name,
+                    style: Theme.of(context).textTheme.headline1!.copyWith(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                  SizedBox(width: 4.0),
+                  if (isNew)
+                    Container(
+                      height: 4.0,
+                      width: 4.0,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: const Color(0xFFDE2B13)),
+                    ),
+                ],
+              ),
+              const SizedBox(height: 2),
+              Text(
+                box.description,
+                style: Theme.of(context).textTheme.headline1!.copyWith(
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+              ),
+            ],
+          ),
+          Text(
+            DateFormat("hh:mm").format(DateTime.parse(box.date)),
+            style: Theme.of(context).textTheme.headline2!.copyWith(
+                  fontSize: 11.0,
+                  fontWeight: FontWeight.w400,
+                ),
+          ),
+        ],
+      ),
     );
   }
 }
