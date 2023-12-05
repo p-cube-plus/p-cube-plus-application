c5c951c96592749517f25d329aa5b114853b24c1
Changhan Oh <ochh508@naver.com>
2023-12-05 22:27:39 +0900

    유저 프로젝트 DTO 오류 수정

--- lib/models/user/user_project.dart
+++ lib/models/user/user_project.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 @JsonSerializable()
 class UserProject {
-  final String id;
+  final int id;
   final String name;
   final String type;
   final String status;
@@ -26,31 +26,29 @@ class UserProject {
     required this.isAbleInquiry,
   });
 
-  factory UserProject.fromJson(Map<String, dynamic> json) {
+  factory UserProject.fromJson(Map<String, dynamic> data) {
+    var platformList = data['platform']
+        .toString()
+        .replaceAll('[', '')
+        .replaceAll(']', '')
+        .split(',')
+        .map<String>((e) {
+      return e.replaceAll(" ", "");
+    }).toList();
+
     return UserProject(
-      id: json['id'].toString(),
-      name: json['name'],
-      type: json['type'],
-      status: json['status'],
-      startDate: DateTime.parse(json['start_date']),
-      endDate: json['end_date'] ?? DateTime.parse(json['end_date']),
-      graphic: json['graphic'],
-      platform: json['platform'],
-      isFindingMember: json['is_finding_member'],
-      isAbleInquiry: json['is_able_inquiry'],
+      id: data['id'],
+      name: data['name'],
+      type: data['type'],
+      status: data['status'],
+      startDate: DateTime.parse(data['start_date']),
+      endDate: data['end_date'] != null
+          ? DateTime.parse(data['end_date'])
+          : data['end_date'],
+      graphic: data['graphic'],
+      platform: platformList,
+      isFindingMember: data['is_finding_member'],
+      isAbleInquiry: data['is_able_inquiry'],
     );
   }
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'type': type,
-        'status': status,
-        'start_date': startDate,
-        'end_date': endDate,
-        'graphic': graphic,
-        'platform': platform,
-        'is_finding_member': isFindingMember,
-        'is_able_inquiry': isAbleInquiry,
-      };
 }
