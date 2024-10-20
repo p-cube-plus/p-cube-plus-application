7d109531e98f050215655fd625229cfb23d3f542
Retrobas <ohminkwon2718@gmail.com>
2024-10-21 01:06:02 +0900

    임원진 알람 재작중

--- presentation/lib/ui/executive/alarm/executive_alarm_setting_page.dart
+++ presentation/lib/ui/executive/alarm/executive_alarm_setting_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:presentation/extensions/theme_data_extension.dart';
 import 'package:presentation/widgets/defauilt_toggle_tile.dart';
 import 'package:presentation/widgets/default_appbar.dart';
 import 'package:presentation/widgets/default_content.dart';
 import 'package:presentation/widgets/default_page.dart';
+import 'package:presentation/widgets/rounded_border.dart';
 
 class ExecutiveAlarmSettingPage extends StatelessWidget {
   const ExecutiveAlarmSettingPage({super.key});
@@ -21,6 +23,32 @@ class ExecutiveAlarmSettingPage extends StatelessWidget {
               title: "알림 켜짐",
               value: true,
               onChanged: (bool isOn) {},
+            ),
+            const SizedBox(height: 16),
+            RoundedBorder(
+              padding:
+                  EdgeInsetsDirectional.symmetric(horizontal: 20, vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "회의",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: theme.neutral60,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    "2시간 전",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: theme.neutral100,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
