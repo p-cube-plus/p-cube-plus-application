bc4a3ccb57201df293744408a284d55cda2cb2c9
Retrobas <ohminkwon2718@gmail.com>
2024-11-27 23:14:19 +0900

    crashlytics의 등장

--- app/lib/initialize_on_app_start.dart
+++ app/lib/initialize_on_app_start.dart
@@ -1,5 +1,22 @@
+import 'package:app/firebase_options.dart';
 import 'package:data/initialize_on_app_start.dart' as data_init;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
-Future initializeOnAppStart() {
-  return data_init.initOnAppStart();
+Future initializeOnAppStart() async {
+  await data_init.initOnAppStart();
+
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,21 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:presentation/p_cube_plus_app.dart';
 import 'regist_service.dart' as ioc;
 import 'initialize_on_app_start.dart' as init;
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   ioc.registService();
   await init.initializeOnAppStart();
-
-  try {
-    if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    }
-  } catch (_) {}
 
   runApp(const PCubePlusApp());
 }
