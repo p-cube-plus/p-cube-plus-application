b5411036e82a50585db780cdff8f4ffada28247a
Ohmin Kwon <ohminkwon2718@gmail.com>
2025-01-27 16:19:45 +0900

    새로고침 및 상태저장 추가

--- presentation/lib/ui/fee/fee_page.dart
+++ presentation/lib/ui/fee/fee_page.dart
@@ -7,6 +7,7 @@ import 'package:presentation/ui/fee/month_fee_status_per_year/month_fee_status_p
 import 'package:presentation/ui/fee/total_fee/total_fee_widget.dart';
 import 'package:presentation/widgets/default_content.dart';
 import 'package:presentation/widgets/default_page.dart';
+import 'package:presentation/widgets/default_refresh_indicator.dart';
 import 'package:provider/provider.dart';
 
 class FeePage extends StatelessWidget {
@@ -21,52 +22,72 @@ class FeePage extends StatelessWidget {
   }
 }
 
-class _FeePage extends StatelessWidget with ViewModel<FeeViewModel> {
+class _FeePage extends StatefulWidget {
   const _FeePage();
 
   @override
+  State<_FeePage> createState() => _FeePageState();
+}
+
+class _FeePageState extends State<_FeePage>
+    with ViewModel<FeeViewModel>, AutomaticKeepAliveClientMixin {
+  Key _refreshKey = UniqueKey();
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     final theme = Theme.of(context);
     return DefaultPage(
+      key: _refreshKey,
       title: "회비",
-      content: DefaultContent(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "이번 달 회비 납부",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                color: theme.neutral100,
+      content: DefaultRefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            _refreshKey = UniqueKey();
+          });
+        },
+        child: DefaultContent(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "이번 달 회비 납부",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: theme.neutral100,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            CurrentMonthFeeWidget(),
-            SizedBox(height: 40),
-            Text(
-              "연도별 회비 납부",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                color: theme.neutral100,
+              SizedBox(height: 8),
+              CurrentMonthFeeWidget(),
+              SizedBox(height: 40),
+              Text(
+                "연도별 회비 납부",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: theme.neutral100,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            MonthFeeStatusPerYearWidget(),
-            SizedBox(height: 40),
-            Text(
-              "회비 내역",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                color: theme.neutral100,
+              SizedBox(height: 8),
+              MonthFeeStatusPerYearWidget(),
+              SizedBox(height: 40),
+              Text(
+                "회비 내역",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  color: theme.neutral100,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            TotalFeeWidget(),
-          ],
+              SizedBox(height: 8),
+              TotalFeeWidget(),
+            ],
+          ),
         ),
       ),
     );
